684ea6f3886d1813ccdc683818af499d98099c21
vuduong5 <duongtanvu096@gmail.com>
2015-11-25 10:26:59 +0700

    loi title window khi hien thi da duoc sua

--- k20jwhiteboard/src/jWhiteBoard/JWhiteBoard.java
+++ k20jwhiteboard/src/jWhiteBoard/JWhiteBoard.java
@@ -293,6 +293,7 @@ public class JWhiteBoard extends ReceiverAdapter implements ActionListener, Chan
             channel.connect(groupName, null, stateTimeout);
         }
         mainFrame.setVisible(true);
+        setTitle();//them dong settitle 
     }
 
     /**
